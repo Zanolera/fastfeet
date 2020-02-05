93b07165101eb38f12db7986244ff7dbbddb0955
Zanolera <marckzanol@gmail.com>
2020-02-05 00:06:05 -0300

    User + Recipient Models/Controllers + recipient migration

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,11 @@
 import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import RecipientController from './app/controllers/RecipientController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.status(400).json({ message: 'ok' }));
+routes.post('/users', UserController.store);
+routes.post('/recipients', RecipientController.store);
 
 export default routes;
